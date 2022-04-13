79ab08a04f01b968b561769d01d9ccc89b465f7a
Eddy Mendoza <eddymy1304@gmail.com>
2022-04-13 12:22:05 -0500

    todos los cambios realizados

--- app/src/main/java/com/eddymy1304/propinas/MainActivity.kt
+++ app/src/main/java/com/eddymy1304/propinas/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.eddymy1304.propinas
 
+import android.icu.text.NumberFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.eddymy1304.propinas.databinding.ActivityMainBinding
@@ -12,6 +13,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.calcular.setOnClickListener {calcularPropina()}
+
+    }
+
+    private fun calcularPropina() {
+        var costo_servicio = binding.costoServicio.text.toString().toDoubleOrNull() ?: 0.00
+        val porcentaje = when(binding.opciones.checkedRadioButtonId){
+            R.id.opcion_veinte_por_ciento -> 0.2
+            R.id.opcion_dieciocho_por_ciento -> 0.18
+            R.id.opcion_quince_por_ciento -> 0.15
+            else -> 0.00
+        }
+        var resultadoPropina = costo_servicio.times(porcentaje)
+
+        if (binding.switchRedondear.isChecked) {
+            resultadoPropina = kotlin.math.ceil(resultadoPropina)
+        }
+
+        val formatoMoneda = NumberFormat.getCurrencyInstance().format(resultadoPropina)
+        binding.resultado.text = getString(R.string.propina, formatoMoneda)
 
 
     }
